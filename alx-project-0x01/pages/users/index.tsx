9ca74f75ff64e9f11d8996a13a02a24e25570aa7
Adejare Faruk <nou183023868@noun.edu.ng>
2025-05-31 09:57:05 +0100

    uddate

---
+++ alx-project-0x01/pages/users/index.tsx
@@ -0,0 +1,11 @@
+import Header from "@/components/layout/Header";
+
+const Users: React.FC = () => {
+    return(
+        <div>
+            <Header/>
+        </div>
+    )
+}
+
+export default Users;
