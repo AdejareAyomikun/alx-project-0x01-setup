8c91f192ebd928457a163fd1161dca616d0699fc
Adejare Faruk <nou183023868@noun.edu.ng>
2025-06-01 17:51:47 +0100

    updated

--- alx-project-0x01/interfaces/index.ts
+++ alx-project-0x01/interfaces/index.ts
@@ -1,11 +1,11 @@
 export interface PostProps {
-   userId: number;
+  userId: number;
   id: number;
   title: string;
   body: string;
 }
 
-export interface UsersProps{
+export interface UserProps {
   id: number;
   name: string;
   username: string;
@@ -27,4 +27,46 @@ export interface UsersProps{
     catchPhrase: string;
     bs: string;
   };
+}
+
+export interface PostData {
+  userId: number;
+  id?: number;
+  title: string;
+  body: string;
+}
+
+export interface PostModalProps {
+  onClose: () => void;
+  onSubmit: (post: PostData) => void;
+}
+
+export interface UserData {
+  id: number;
+  name: string;
+  username: string;
+  email: string;
+  address: {
+    street: string;
+    suite: string;
+    city: string;
+    zipcode: string;
+    geo: {
+      lat: string;
+      lng: string;
+    };
+  };
+  phone: string;
+  website: string;
+  company: {
+    name: string;
+    catchPhrase: string;
+    bs: string;
+  };
+}
+
+export interface UserModalProps {
+  isOpen: boolean;
+  onClose: () => void;
+  onSubmit: (post: UserProps) => void;
 }
